12bb8904f5c9fc5301c4dccf61154922c4d86fe1
João Pedro <mjoaopedro61@gmail.com>
2020-06-23 17:32:23 -0300

    Buildable

--- publish/esm5/version/version.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * Generated from: version.ts
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-import { Version } from '@angular/core';
-/** @type {?} */
-export var VERSION = new Version('0.0.1');
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmVyc2lvbi5qcyIsInNvdXJjZVJvb3QiOiJuZzovL3l1ZS11aS92ZXJzaW9uLyIsInNvdXJjZXMiOlsidmVyc2lvbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLE9BQU8sRUFBRSxPQUFPLEVBQUUsTUFBTSxlQUFlLENBQUM7O0FBRXhDLE1BQU0sS0FBTyxPQUFPLEdBQUcsSUFBSSxPQUFPLENBQUMsT0FBTyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgVmVyc2lvbiB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5leHBvcnQgY29uc3QgVkVSU0lPTiA9IG5ldyBWZXJzaW9uKCcwLjAuMScpO1xuIl19
