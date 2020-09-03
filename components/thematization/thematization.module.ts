c792d58544b01428a03f0e4ca526068c5fdeb0a2
Joao Pedro <mjoaopedro61@gmail.com>
2020-09-03 13:16:09 -0300

    Bit changes

--- components/thematization/thematization.module.ts
+++ components/thematization/thematization.module.ts
@@ -1,15 +1,14 @@
 import { NgModule, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
 
 import { logging } from '@JoaoPedro61/yue-ui/core/utils';
 import { VERSION } from '@JoaoPedro61/yue-ui/version';
 
-import { YUE_UI_THEME_DEFAULT, YUE_UI_THEME } from './thematization.utils';
+import { YUE_UI_THEME_DEFAULT, YUE_UI_THEME, YUE_UI_THEME_BEHAVIOR } from './thematization.utils';
 
 import { DEFAULT_LIGHT_THEME } from './thematization.themes';
 
-import { YueUiThemeDefault, YueUiThemeConfig } from './thematization.interfaces';
+import { YueUiThemeDefault } from './thematization.interfaces';
 import { YueUiThematizationService } from './thematization.service';
 
 
@@ -30,7 +29,7 @@ const logger = logging.getLogger('core.thematization');
     },
     {
       provide: YUE_UI_THEME,
-      useValue: new BehaviorSubject<YueUiThemeConfig>(DEFAULT_LIGHT_THEME)
+      useValue: YUE_UI_THEME_BEHAVIOR,
     },
     YueUiThematizationService
   ]
@@ -38,7 +37,8 @@ const logger = logging.getLogger('core.thematization');
 export class YueUiThematizationModule {
 
   constructor(@Inject(YUE_UI_THEME_DEFAULT) private readonly _default: YueUiThemeDefault, private readonly _service: YueUiThematizationService) {
-    logger.info(`YueUiThematizationModule on version: ${VERSION.full}`);    
+    logger.info(`YueUiThematizationModule on version: ${VERSION.full}`);
+
     if (this._default !== 'light') {
       this._service.setTheme(this._default);
     }
--- components/thematization/thematization.utils.ts
+++ components/thematization/thematization.utils.ts
@@ -1,8 +1,12 @@
 import { InjectionToken } from '@angular/core';
 
 import { YueUiTheme, YueUiThemeDefault } from './thematization.interfaces';
+import { BehaviorSubject } from 'rxjs';
+import { DEFAULT_LIGHT_THEME } from './thematization.themes';
 
 
 export const YUE_UI_THEME: InjectionToken<YueUiTheme> = new InjectionToken(`YUE_UI_THEME`);
 
 export const YUE_UI_THEME_DEFAULT: InjectionToken<YueUiThemeDefault> = new InjectionToken(`YUE_UI_THEME_DEFAULT`);
+
+export const YUE_UI_THEME_BEHAVIOR: YueUiTheme = new BehaviorSubject(DEFAULT_LIGHT_THEME);
